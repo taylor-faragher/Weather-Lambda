fb07e2d8379fd1b00792a56e189f5393f4b11d13
Taylor Faragher <faragher6@gmail.com>
2024-05-06 23:42:01 -0500

    Updates stack to use environment variables for values

--- bin/app-cdk.ts
+++ bin/app-cdk.ts
@@ -1,12 +1,18 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { WeatherByZipCodeStack } from '../lib/weatherByZipCodeStack';
+import { getCdkConfig } from './getCdkConfig';
 
 const app = new cdk.App();
 
-new WeatherByZipCodeStack(app, 'WeatherLambdaStack', {
+const env = app.node.tryGetContext('env') || 'dev'; 
+
+const config = getCdkConfig(env);
+
+new WeatherByZipCodeStack(app, `WeatherLambdaStack-${env}`, {
+    customConfig: config,
     env: {
         account: process.env.AWS_ACCOUNT_NUMBER,
-        region: process.env.REGION
+        region: config.region
     }
 });
