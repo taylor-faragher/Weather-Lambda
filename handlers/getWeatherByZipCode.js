fd3921be3fd38b347638d54083a09133bedb964c
Taylor Faragher <faragher6@gmail.com>
2024-05-28 12:27:11 -0500

    Linter format changes

--- handlers/getWeatherByZipCode.js
+++ handlers/getWeatherByZipCode.js
@@ -1,19 +1,21 @@
-exports.getWeatherByZipCode = async (zipCode) => {
+exports.getWeatherByZipCode = async zipCode => {
     const key = process.env.API_KEY;
     try {
-        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${key}&units=imperial`)
+        const res = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${key}&units=imperial`
+        );
         const data = await res.json();
         if (!res.ok) {
-            console.log(`OpenWeatherMap threw an error getting the weather: ${JSON.stringify(data)}`)
+            console.log(`OpenWeatherMap threw an error getting the weather: ${JSON.stringify(data)}`);
         } //allows me to see openweathermap errors in cloudwatch
         return {
             statusCode: res.status,
             body: JSON.stringify(data),
             headers: {
-                "Access-Control-Allow-Origin": "*",
-            }
-        }
+                'Access-Control-Allow-Origin': '*',
+            },
+        };
     } catch (error) {
-        console.log(`There was an error fetching the weather in the lambda: ${error}`)
+        console.log(`There was an error fetching the weather in the lambda: ${error}`);
     }
-}
+};
--- handlers/weather.js
+++ handlers/weather.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const weatherService = require('./getWeather');
 const weatherByZipCodeService = require('./getWeatherByZipCode');
 
