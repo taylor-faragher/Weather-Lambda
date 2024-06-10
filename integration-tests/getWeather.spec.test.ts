4241af57e7b2f598e5924f883ecffa1a4d833ea1
Taylor Faragher <faragher6@gmail.com>
2024-06-09 19:46:40 -0500

    Adds api key to unit test; adds scenario for no api key

--- integration-tests/getWeather.spec.test.ts
+++ integration-tests/getWeather.spec.test.ts
@@ -5,6 +5,7 @@ describe('taylorsweatherapi.com', () => {
     it('GET with no params returns weather for default city, Washington DC', done => {
         request(baseUrl)
             .get('/')
+            .set('x-api-key', 'uFaywcvpQj94HeeNC8ESd3w5eSS8NcxKaILjvkVq')
             .expect(200)
             .end((err, res) => {
                 if (err) return done(err);
@@ -24,6 +25,7 @@ describe('taylorsweatherapi.com', () => {
     it('GET with zipcode param returns weather for specific city', done => {
         request(baseUrl)
             .get('/?zipcode=37167')
+            .set('x-api-key', 'uFaywcvpQj94HeeNC8ESd3w5eSS8NcxKaILjvkVq')
             .expect(200)
             .end((err, res) => {
                 if (err) return done(err);
@@ -42,6 +44,7 @@ describe('taylorsweatherapi.com', () => {
     it('GET with partial zipcode param returns a 404 city not found message', async () => {
         request(baseUrl)
             .get('/?zipcode=371')
+            .set('x-api-key', 'uFaywcvpQj94HeeNC8ESd3w5eSS8NcxKaILjvkVq')
             .expect(404)
             .then(res => {
                 expect(res.body).toHaveProperty('cod');
@@ -57,6 +60,7 @@ describe('taylorsweatherapi.com', () => {
     it('GET with empty zipcode param returns a 400 bad request', async () => {
         request(baseUrl)
             .get('/?zipcode=')
+            .set('x-api-key', 'uFaywcvpQj94HeeNC8ESd3w5eSS8NcxKaILjvkVq')
             .expect(400)
             .then(res => {
                 expect(res.body.error).toEqual('Bad Request. Please send correct data.');
@@ -69,6 +73,7 @@ describe('taylorsweatherapi.com', () => {
     it('GET with random param returns a 400 bad request', async () => {
         request(baseUrl)
             .get('/?random=')
+            .set('x-api-key', 'uFaywcvpQj94HeeNC8ESd3w5eSS8NcxKaILjvkVq')
             .expect(400)
             .then(res => {
                 expect(res.body.error).toEqual('Bad Request. Please send correct data.');
@@ -81,6 +86,7 @@ describe('taylorsweatherapi.com', () => {
     it('GET with city and state params returns a 200 request with no weather', async () => {
         request(baseUrl)
             .get('?city=Nashville&state=TN')
+            .set('x-api-key', 'uFaywcvpQj94HeeNC8ESd3w5eSS8NcxKaILjvkVq')
             .expect(200)
             .then(res => {
                 expect(res.body.message).toEqual('Not Implemented yet!');
@@ -93,9 +99,22 @@ describe('taylorsweatherapi.com', () => {
     it('GET with city params ONLY returns a 400 bad request', async () => {
         request(baseUrl)
             .get('?city=Nashville')
+            .set('x-api-key', 'uFaywcvpQj94HeeNC8ESd3w5eSS8NcxKaILjvkVq')
             .expect(400)
             .then(res => {
                 expect(res.body.error).toEqual('Bad Request. Please send correct data.');
+            })
+            .catch(err => {
+                throw err;
+            });
+    });
+
+    it('GET without API key in header returns 403 Forbidden', async () => {
+        request(baseUrl)
+            .get('/?zipcode=37167')
+            .expect(403)
+            .then(res => {
+                expect(res.body.message).toEqual('Forbidden');
             })
             .catch(err => {
                 throw err;
