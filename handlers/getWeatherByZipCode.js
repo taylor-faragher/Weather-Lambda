b59606dd0df177a5294220d5ee1d9e40e09a5378
Taylor Faragher <faragher6@gmail.com>
2024-06-10 00:08:31 -0500

    Adds if statement to avoid mapping 400 errors

--- handlers/getWeatherByZipCode.js
+++ handlers/getWeatherByZipCode.js
@@ -8,17 +8,26 @@ exports.getWeatherByZipCode = async zipCode => {
             `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${key}&units=imperial`
         );
         const data = await res.json();
-        if (!res.ok) {
+        if (res.status !== 200) {
+            //allows me to see openweathermap errors in cloudwatch
             console.log(`OpenWeatherMap threw an error getting the weather: ${JSON.stringify(data)}`);
-        } //allows me to see openweathermap errors in cloudwatch
-        const mappedData = mapper.mapWeatherData(data);
-        return {
-            statusCode: res.status,
-            body: JSON.stringify(mappedData),
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-            },
-        };
+            return {
+                statusCode: res.status,
+                body: JSON.stringify(data),
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                },
+            };
+        } else {
+            const mappedData = mapper.mapWeatherData(data);
+            return {
+                statusCode: res.status,
+                body: JSON.stringify(mappedData),
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                },
+            };
+        }
     } catch (error) {
         console.log(`There was an error fetching the weather in the lambda: ${error}`);
     }
