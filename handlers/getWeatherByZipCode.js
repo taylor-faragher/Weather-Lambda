9c604973d2aa620cbd7f628605cc89db4d39ff7c
Taylor Faragher <faragher6@gmail.com>
2024-06-17 14:03:24 -0500

    Updates main handler to use two API calls for zipcode now. Updates to new api for weather data; uses updated mapper

--- handlers/getWeatherByZipCode.js
+++ handlers/getWeatherByZipCode.js
@@ -1,27 +1,34 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const mapper = require('./utils/mapWeatherData');
+import {mapWeatherData} from './utils/mapWeatherData';
 
-exports.getWeatherByZipCode = async zipCode => {
+export async function getWeatherByZipCode(zipCode) {
     const key = process.env.API_KEY;
     try {
-        const res = await fetch(
-            `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${key}&units=imperial`
+        const geolocationResponse = await fetch(
+            `http://api.openweathermap.org/geo/1.0/zip?zip=${zipCode},US&appid=${key}`
         );
-        const data = await res.json();
-        if (res.status !== 200) {
+        const data = await geolocationResponse.json();
+        const lat = data.lat;
+        const lon = data.lon;
+
+        const oneCall = await fetch(
+            `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely&appid=${key}&units=imperial`
+        );
+        const oneCallData = await oneCall.json();
+
+        if (geolocationResponse.status !== 200 || oneCall.status !== 200) {
             //allows me to see openweathermap errors in cloudwatch
             console.log(`OpenWeatherMap threw an error getting the weather: ${JSON.stringify(data)}`);
             return {
-                statusCode: res.status,
+                statusCode: geolocationResponse.status,
                 body: JSON.stringify(data),
                 headers: {
                     'Access-Control-Allow-Origin': '*',
                 },
             };
         } else {
-            const mappedData = mapper.mapWeatherData(data);
+            const mappedData = await mapWeatherData(oneCallData);
             return {
-                statusCode: res.status,
+                statusCode: oneCall.status,
                 body: JSON.stringify(mappedData),
                 headers: {
                     'Access-Control-Allow-Origin': '*',
@@ -31,4 +38,4 @@ exports.getWeatherByZipCode = async zipCode => {
     } catch (error) {
         console.log(`There was an error fetching the weather in the lambda: ${error}`);
     }
-};
+}
