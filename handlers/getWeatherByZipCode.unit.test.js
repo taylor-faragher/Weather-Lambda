309fa219bfa96824e4b1d30a08c7da6e3d2944fa
Taylor Faragher <faragher6@gmail.com>
2024-06-17 20:32:23 -0500

    Updates unit tests for getWeather by zip code

--- handlers/getWeatherByZipCode.unit.test.js
+++ handlers/getWeatherByZipCode.unit.test.js
@@ -1,80 +1,133 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const {getWeatherByZipCode} = require('./getWeatherByZipCode');
-const mapper = require('./utils/mapWeatherData');
-
-global.fetch = jest.fn(() =>
-    Promise.resolve({
-        json: () => Promise.resolve({weather: 'sunny'}),
-        status: 200,
-    })
-);
+import {getWeatherByZipCode} from './getWeatherByZipCode';
+import {mapWeatherData} from './utils/mapWeatherData';
 
 jest.mock('./utils/mapWeatherData');
 
 describe('getWeatherByZipCode', () => {
-    beforeEach(() => {
-        jest.clearAllMocks();
-        process.env.API_KEY = 'fake_api_key';
+    const API_KEY = 'test-api-key';
+    const ZIP_CODE = '20001';
+    const MOCK_GEOLOCATION_DATA = {lat: 38.9067, lon: -77.0312};
+    const MOCK_WEATHER_DATA = {weather: 'sunny'};
+    const MAPPED_DATA = {temperature: 70};
+
+    beforeAll(() => {
+        process.env.API_KEY = API_KEY;
     });
 
-    it('should return weather data when fetch is successful', async () => {
-        const mockZipCode = '20005';
-        const mockData = {weather: 'sunny'};
-        const mockMappedData = {weather: 'mapped sunny'};
+    beforeEach(() => {
+        jest.clearAllMocks();
+        global.fetch = jest.fn();
+    });
 
-        fetch.mockResolvedValue({
-            ok: true,
-            status: 200,
-            json: jest.fn().mockResolvedValue(mockData),
-        });
+    it('should return weather data on successful fetches', async () => {
+        global.fetch
+            .mockImplementationOnce(() =>
+                Promise.resolve({
+                    json: () => Promise.resolve(MOCK_GEOLOCATION_DATA),
+                    status: 200,
+                })
+            )
+            .mockImplementationOnce(() =>
+                Promise.resolve({
+                    json: () => Promise.resolve(MOCK_WEATHER_DATA),
+                    status: 200,
+                })
+            );
 
-        mapper.mapWeatherData.mockReturnValue(mockMappedData);
+        mapWeatherData.mockResolvedValue(MAPPED_DATA);
 
-        const result = await getWeatherByZipCode(mockZipCode);
+        const result = await getWeatherByZipCode(ZIP_CODE);
 
-        expect(fetch).toHaveBeenCalledWith(
-            `https://api.openweathermap.org/data/2.5/weather?zip=${mockZipCode}&appid=fake_api_key&units=imperial`
+        expect(global.fetch).toHaveBeenCalledTimes(2);
+        expect(global.fetch).toHaveBeenCalledWith(
+            `http://api.openweathermap.org/geo/1.0/zip?zip=${ZIP_CODE},US&appid=${API_KEY}`
         );
-        expect(mapper.mapWeatherData).toHaveBeenCalledWith(mockData);
+        expect(global.fetch).toHaveBeenCalledWith(
+            `https://api.openweathermap.org/data/3.0/onecall?lat=${MOCK_GEOLOCATION_DATA.lat}&lon=${MOCK_GEOLOCATION_DATA.lon}&exclude=minutely&appid=${API_KEY}&units=imperial`
+        );
+        expect(mapWeatherData).toHaveBeenCalledWith(MOCK_WEATHER_DATA);
         expect(result).toEqual({
             statusCode: 200,
-            body: JSON.stringify(mockMappedData),
+            body: JSON.stringify(MAPPED_DATA),
             headers: {
                 'Access-Control-Allow-Origin': '*',
             },
         });
     });
 
-    it('should log an error and not throw if fetch fails', async () => {
-        const consoleLogSpy = jest.spyOn(console, 'log').mockImplementation(() => {});
-        const mockZipCode = '20005';
-
-        fetch.mockRejectedValue(new Error('Fetch failed'));
-
-        await getWeatherByZipCode(mockZipCode);
-
-        expect(consoleLogSpy).toHaveBeenCalledWith(
-            'There was an error fetching the weather in the lambda: Error: Fetch failed'
+    it('should log and return error response when geolocation fetch fails with non-200 status', async () => {
+        global.fetch.mockImplementationOnce(() =>
+            Promise.resolve({
+                json: () => Promise.resolve({message: 'Not Found'}),
+                status: 404,
+            })
         );
+
+        const consoleLogSpy = jest.spyOn(console, 'log').mockImplementation(() => {});
+
+        const result = await getWeatherByZipCode(ZIP_CODE);
+
+        const expectedMessage = 'OpenWeatherMap threw an error getting the weather';
+
+        expect(consoleLogSpy).toHaveBeenCalledWith(expectedMessage);
+        expect(result).toEqual({
+            statusCode: 404,
+            body: JSON.stringify({message: `OpenWeatherMap threw an error getting the weather: {}`}),
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+            },
+        });
+
         consoleLogSpy.mockRestore();
     });
 
-    it('should log an OpenWeatherMap error if the response is not ok', async () => {
-        const mockZipCode = '20005';
-        const mockData = {message: 'Not Found'};
+    it('should log and return error response when one call fetch fails with non-200 status', async () => {
+        global.fetch
+            .mockImplementationOnce(() =>
+                Promise.resolve({
+                    json: () => Promise.resolve(MOCK_GEOLOCATION_DATA),
+                    status: 200,
+                })
+            )
+            .mockImplementationOnce(() =>
+                Promise.resolve({
+                    json: () => Promise.resolve({message: 'Not Found'}),
+                    status: 404,
+                })
+            );
+
         const consoleLogSpy = jest.spyOn(console, 'log').mockImplementation(() => {});
 
-        fetch.mockResolvedValue({
-            ok: false,
-            status: 404,
-            json: jest.fn().mockResolvedValue(mockData),
+        const result = await getWeatherByZipCode(ZIP_CODE);
+
+        const expectedMessage = 'OpenWeatherMap threw an error getting the weather';
+
+        expect(consoleLogSpy).toHaveBeenCalledWith(expectedMessage);
+        expect(result).toEqual({
+            statusCode: 404,
+            body: JSON.stringify({
+                message: `OpenWeatherMap threw an error getting the weather: {"message":"Not Found"}`,
+            }),
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+            },
         });
 
-        await getWeatherByZipCode(mockZipCode);
+        consoleLogSpy.mockRestore();
+    });
+
+    it('should log error when fetch throws an exception', async () => {
+        const errorMessage = 'Network Error';
+        global.fetch.mockImplementationOnce(() => Promise.reject(new Error(errorMessage)));
+
+        const consoleLogSpy = jest.spyOn(console, 'log').mockImplementation(() => {});
+
+        await getWeatherByZipCode(ZIP_CODE);
 
         expect(consoleLogSpy).toHaveBeenCalledWith(
-            'OpenWeatherMap threw an error getting the weather: {"message":"Not Found"}'
+            `There was an error fetching the weather in the lambda: Error: ${errorMessage}`
         );
+
         consoleLogSpy.mockRestore();
     });
 });
