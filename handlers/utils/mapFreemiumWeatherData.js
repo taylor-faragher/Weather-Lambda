5c26bf0413845471700bada890f0fb0f6323579c
Taylor Faragher <faragher6@gmail.com>
2024-06-24 17:06:28 -0500

    Adds mapper for freemium data

---
+++ handlers/utils/mapFreemiumWeatherData.js
@@ -0,0 +1,25 @@
+const mapFreemiumWeatherData = async (data, city = 'Washington') => {
+    if (!data) {
+        return;
+    }
+
+    const mappedData = {
+        coordinates: {
+            lon: data.lon,
+            lat: data.lat,
+        },
+        current: {
+            temperature: data.current.temp,
+            icon: data.current.weather[0].icon,
+            description: data.current.weather[0].description,
+        },
+        area: {
+            timezone: data.timezone,
+            majorCity: city,
+        },
+    };
+
+    return mappedData;
+};
+
+export default mapFreemiumWeatherData;
